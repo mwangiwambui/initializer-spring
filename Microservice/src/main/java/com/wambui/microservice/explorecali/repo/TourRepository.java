21ae276427c46b27885982e10a98a6524604eb76
mwangiwambui <wambui54mwangi@gmail.com>
2023-02-07 07:35:15 +0300

    Addition of methods at the repository level

--- Microservice/src/main/java/com/wambui/microservice/explorecali/repo/TourRepository.java
+++ Microservice/src/main/java/com/wambui/microservice/explorecali/repo/TourRepository.java
@@ -1,7 +1,23 @@
 package com.wambui.microservice.explorecali.repo;
 
+import com.wambui.microservice.explorecali.domain.Difficulty;
+import com.wambui.microservice.explorecali.domain.Region;
 import com.wambui.microservice.explorecali.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
 public interface TourRepository extends CrudRepository<Tour, Integer> {
+    List<Tour> findByTourPackageCode(String code);
+
+    Optional<Tour> findByTitle(String title);
+
+    List<Tour> findByPrice(Integer price);
+
+    Collection<Tour> findByDifficulty(Difficulty difficulty);
+
+    List<Tour> findByRegion(Region region);
 }
